6531281cdfe21d3eef06559301905904dc09d05e
Pench1k <Denis.machkarin@gmail.com>
2024-12-21 22:21:37 +0300

    Roles

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,47 +1,99 @@
 using BD.DBContext;
+using BD.Entity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MVC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Smile"));
 });
+
+// Add Seed Service
 builder.Services.AddTransient<Seed>();
+
+// Add Identity
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure SignInManager
+builder.Services.AddScoped<SignInManager<User>>();
+
+// Add Authorization Policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("StudentPolicy", policy => policy.RequireRole("Студент"));
+    options.AddPolicy("TeacherPolicy", policy => policy.RequireRole("Преподаватель"));
+    options.AddPolicy("DekanPolicy", policy => policy.RequireRole("Декан"));
+    options.AddPolicy("DeputyPolicy", policy => policy.RequireRole("Заместитель кафедры"));
+});
+
+// Configure Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+        options.LoginPath = "/Home/Login";
+        options.AccessDeniedPath = "/Home/Index";
+        options.SlidingExpiration = true;
+        options.ReturnUrlParameter = string.Empty;
+    });
+
 var app = builder.Build();
 
+// Run Seed Data
 if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
     SeedData(app);
+}
 
 void SeedData(IHost app)
 {
-    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-
-    using (var scope = scopedFactory.CreateScope())
+    using (var scope = app.Services.CreateScope())
     {
         var service = scope.ServiceProvider.GetService<Seed>();
         service.SeedDataContext();
     }
 }
 
-// Configure the HTTP request pipeline.
+// Configure Role Manager
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roleNames = { "Врач", "Пациент", "Админ"};
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
+
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
